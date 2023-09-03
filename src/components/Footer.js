c9a077788ee23c23288c7c2754319a085b58e913
Marcus <marcus.vinicius.bittencourt.c@gmail.com>
2023-09-03 16:49:56 -0300

    Finalizadas todas as sessões

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,21 +1,25 @@
 import '../styles/footer.css'
+import { FaInstagram, FaWhatsapp } from 'react-icons/fa'
 
 function Footer() {
     return <footer>
         <a href="#d" className="footer_logo">Marcus Costa</a>
 
         <ul className='permalinks'>
-            <li href="#header">Home</li>
-            <li href="#about">About</li>
-            <li href="#projects">Projetos</li>
-            <li href="#header">Experiência</li>
-            <li href="#header">Contato</li>
+            <li href="#header"><a href="#header">Home</a></li>
+            <li href="#about"><a href="#about">About</a></li>
+            <li href="#projects"><a href="#projects">Projetos</a></li>
+            <li href="#header"><a href="#experience">Experiência</a></li>
+            <li href="#header"><a href="#contact">Contato</a></li>
         </ul>
 
         <div className='footer_socials'>
-            <a href='#linkedin.com'>Linkedin</a>
-            <a href='#linkedin.com'>Linkedin</a>
-            <a href='#linkedin.com'>Linkedin</a>
+            <a href='https://facebook.com'><FaInstagram /></a>
+            <a href=''><FaWhatsapp /></a>
+        </div>
+
+        <div className='footer_copyright'>
+            <small>&copy; Marcus Costa. All rights reserved.</small>
         </div>
     </footer>
 }
